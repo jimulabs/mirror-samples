12fe91393843d295e94c9af744bf38d30199f559
lintonye <linton@jimulabs.com>
2014-07-19 17:43:07 -0700

    sample code for window animations

--- TuneInRedesign/app/mirror/js/home.js
+++ TuneInRedesign/app/mirror/js/home.js
@@ -7,9 +7,32 @@ var openNowPlaying = function() {
         duration: 500
     }
 //    openScreen('../now_playing.xml', slideLeftIn, slideLeftOut)
+
+/*
+    // using the old view animations (which can be used in overridePendingAnimations())
+    openScreen('../now_playing.xml',
+        '@anim/slide_up_in', '@anim/slide_up_out',
+        '@anim/slide_down_in', '@anim/slide_down_out')
+
+    // using property animations
     openScreen('../now_playing.xml',
         '@animator/slide_up_in', '@animator/slide_up_out',
         '@animator/slide_down_in', '@animator/slide_down_out')
+
+    // A null param means no corresponding animation
+    // here, the new screen will slide in when launched, and slide out when Back is pressed
+    // however, the calling screen won't move
+    openScreen('../now_playing.xml',
+        '@anim/slide_up_in', null, null, '@anim/slide_down_out')
+
+    // An omitted param means null
+    openScreen('../now_playing.xml', '@anim/slide_up_in', '@anim/slide_up_out')
+*/
+
+    openScreen('../now_playing.xml',
+        '@anim/slide_up_in', null,
+        null, '@anim/slide_down_out')
+
 }
 $('@id/thumbnail').on('click', openNowPlaying)
 $('@id/title').on('click', openNowPlaying)
