f0dce7a0ed85d1c35f168821dc42f9130348933a
lintonye <linton@jimulabs.com>
2014-07-31 11:39:13 -0700

    Add comments to fancy_animation_choreography

--- PaintExamples/app/mirror/js/fancy_animation_choreography.js
+++ PaintExamples/app/mirror/js/fancy_animation_choreography.js
@@ -7,6 +7,8 @@ for (i=1; i<5; i++) {
 var c1x = $('c1').x
 var c1y = $('c1').y
 
+// set up animations for circles
+
 var moveC1 = $('c1').animate({
     properties: {
         x: [c1x, c1x + 300],
@@ -15,7 +17,7 @@ var moveC1 = $('c1').animate({
     duration: 500
 })
 var scaleC1 = $('c1').animate({
-    properties: { scale: [1, 7.5] }
+    properties: { scale: [1, 8.2] }
 })
 var c1 = together([moveC1, delay(scaleC1, 200)])
 
@@ -31,8 +33,9 @@ var shrinkC3 = delay($('c3').animate(shrinkCircle), cDelay)
 var shrinkC4 = delay($('c4').animate(shrinkCircle), cDelay*2)
 
 var c234 = together([shrinkC2, shrinkC3, shrinkC4])
-
 var c1234 = together([c1, c234])
+
+// set up animations for squares, using a loop
 
 var s1y = $('s1').y
 var showSquare = {
