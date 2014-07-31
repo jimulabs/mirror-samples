7546c16a4423618e02e2dd5d67757a16eafcdba1
lintonye <linton@jimulabs.com>
2014-07-30 18:02:20 -0700

    latest shared view animation customization code

--- TuneInRedesign/app/mirror/js/nowplaying.js
+++ TuneInRedesign/app/mirror/js/nowplaying.js
@@ -8,38 +8,39 @@ function makeAnim(props) {
   }
 }
 
-var rootY = [0, $('/').height - 180]
+var rootY = [0, $('/').height - 150]
 var actionbarAlpha = [1, 0]
 var contentY = [$('#content').y, 0]
+$('time').alpha = 0
+$('title').alpha = 0
+$('subtitle').alpha = 0
 
-function playAnims(index) {
-    var from = index==0 ? 1 : 0
-    var to = index==0 ? 0 : 1
+function createAnimators(isClosing) {
+    var from = isClosing ? 0 : 1
+    var to = isClosing ? 1 : 0
     var root = $('/').animate(makeAnim({y: [rootY[from], rootY[to]]}))
     var content = $('#content').animate(makeAnim({y: [contentY[from], contentY[to]]}))
     var ab = $('#actionbar').animate(makeAnim({alpha: [actionbarAlpha[from], actionbarAlpha[to]]}))
-    var window = together([root,content,ab])
-    $('time').alpha = 0
-    $('title').alpha = 0
-    $('subtitle').alpha = 0
-    var time = $('time').animate({properties: {alpha: [0,1]}})
-    var title = $('title').animate({properties: {alpha: [0,1]}})
-    var subtitle = $('subtitle').animate({properties: {alpha: [0,1]}})
+    var window = together([root,content,ab]) // window = `[root, content, ab]`
+    var labelDuration = 200
+    var fromAlpha = isClosing ? 1 : 0
+    var toAlpha = isClosing ? 0 : 1
+    var labelAnim = {properties: {alpha: [fromAlpha, toAlpha]}, duration: labelDuration}
+    var time = $('time').animate(labelAnim)
+    var title = $('title').animate(labelAnim)
+    var subtitle = $('subtitle').animate(labelAnim)
     var labels = together([title, subtitle, time])
-    sequence([window, labels]).start()
+    return isClosing ? together([labels, window]) : sequence([window, labels]) // return `[root, content, ab] => [title, subtitle, time]`
 }
 
-playAnims(0)
+customizeScreenOpeningAnimation(function(anim) {
+    return together([anim, createAnimators(0)])
+})
 
-/*
-var absX = $('thumbnail').absX
-var absY = $('thumbnail').absY
-var t = $('@id/thumbnail').animate({properties: {scale:[0.2, 1], absX:absX, absY:absY}})
-var s = $('@id/title').animate({properties: {textSize:[1, 80]}})
-together([t,s]).start()
-*/
+customizeScreenClosingAnimation(function(anim) {
+    return together([anim, createAnimators(1)])
+})
 
 $('@android:id/home').parent.on('click', function() {
-    playAnims(1)
     closeThisScreen()
 })
