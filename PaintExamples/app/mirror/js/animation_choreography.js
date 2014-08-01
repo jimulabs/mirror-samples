19f68248b48bd7e2604f7d7a9fb8f386d328c0f3
lintonye <linton@jimulabs.com>
2014-07-31 20:14:55 -0700

    Enable the simplest example

--- PaintExamples/app/mirror/js/animation_choreography.js
+++ PaintExamples/app/mirror/js/animation_choreography.js
@@ -1,6 +1,6 @@
 var v1 = $('view1').animate('@animator/rotate')
 var v2 = $('view2').animate('@animator/rotate')
 
-//sequence([v1, v2]).start()
+sequence([v1, v2]).start()
 //together([v1, v2]).start()
-together([v1, delay(v2, 500)]).start()
+//together([v1, delay(v2, 500)]).start()
