7102d49a1adf20f74123f407fc40a61f5df83742
lintonye <linton@jimulabs.com>
2014-08-03 11:06:58 -0700

    Timely: calls start after animator()

--- TimelyMock/timely/mirror/js/add_alarm.js
+++ TimelyMock/timely/mirror/js/add_alarm.js
@@ -14,10 +14,8 @@ function playRipple(x, y) {
             alpha: 0
         },
         duration: 601
-    });
+    }).start();
 }
-
-playRipple(200, 200)
 
 function calcTimeTextSideX(touchX) {
     var timeBar = $('@id/timeBar')
@@ -74,15 +72,16 @@ timeBarParent.on('touch',
             if (hitTimeBarWhenDown) {
                 var sideX = calcTimeTextSideX(event.x)
                 var centerX = (timeBar.width - timeText.width)/2
+                var textX = timeText.x
                 timeText.animator({
                     properties: {
-                        x: isUp ? centerX : sideX,
-                        scale: isUp ? 1 : 0.8
+                        x: [textX, isUp ? centerX : sideX],
+                        scale: [1, isUp ? 1 : 0.8]
                     },
                     interpolator: '@android:interpolator/decelerate_cubic',
                     duration: 350
-                });
-                if (isUp) timeBar.animator('@animator/bounce_y')
+                }).start();
+                if (isUp) timeBar.animator('@animator/bounce_y').start()
             } else if (isUp) {
                 var sign = event.y < timeBar.y ? -1 : 1
                 setTimeInMinutes(gCurrentMinutes + sign*5)
