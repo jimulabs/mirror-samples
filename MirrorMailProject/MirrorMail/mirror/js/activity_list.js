53c73cb27c93a3dfa280fbda9c91230a2d5eb776
lintonye <linton@jimulabs.com>
2014-08-18 15:53:03 -0700

    MirrorMail: use short reference

--- MirrorMailProject/MirrorMail/mirror/js/activity_list.js
+++ MirrorMailProject/MirrorMail/mirror/js/activity_list.js
@@ -1,21 +1,21 @@
-$('@id/avatar').on('click',
+$('avatar').on('click',
      function(view, event) {
-         view.animator('@animator/card_flip_right_out',
+         view.animator('card_flip_right_out',
              function(animationEvent) {
-                 var tick = $('@id/tick', view.parent)
+                 var tick = $('tick', view.parent)
                  tick.visibility = 'visible'
                  view.visibility = 'invisible'
-                 tick.animator('@animator/card_flip_left_in').start()
+                 tick.animator('card_flip_left_in').start()
             }).start()
      })
 
-$('@id/tick').on('click',
+$('tick').on('click',
     function(view, event) {
-        view.animator('@animator/card_flip_right_out',
+        view.animator('card_flip_right_out',
             function(animationEvent) {
-                var avatar = $('@id/avatar', view.parent)
+                var avatar = $('avatar', view.parent)
                 avatar.visibility = 'visible'
                 view.visibility = 'invisible'
-                avatar.animator('@animator/card_flip_right_in').start()
+                avatar.animator('card_flip_right_in').start()
            }).start()
     })
