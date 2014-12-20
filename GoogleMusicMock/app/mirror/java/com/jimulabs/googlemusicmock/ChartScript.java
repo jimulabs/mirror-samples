915378e39de0e993d221cc2444c1386aa6c40e14
Linton Ye <linton@jimulabs.com>
2014-12-20 15:29:21 -0800

    Add MPChart sample

---
+++ GoogleMusicMock/app/mirror/java/com/jimulabs/googlemusicmock/ChartScript.java
@@ -0,0 +1,71 @@
+package com.jimulabs.googlemusicmock;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+
+import com.github.mikephil.charting.charts.BarLineChartBase;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.LimitLine;
+import com.jimulabs.mirroranimator.MirrorAnimatorScript;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lintonye on 2014-12-20.
+ */
+public class ChartScript extends MirrorAnimatorScript {
+    public ChartScript(Context context, View root) {
+        super(context, root);
+    }
+
+    public ChartScript(View root) {
+        super(root);
+    }
+
+    @Override
+    protected void enterSandbox() {
+        LineChart chart = (LineChart) $("chart").getView();
+        setData(chart, 10, 80);
+    }
+
+    private void setData(LineChart chart, int count, float range) {
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add((i) + "");
+        }
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult) + 3;// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            yVals.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+        set1.setColor(ColorTemplate.getHoloBlue());
+        set1.setCircleColor(ColorTemplate.getHoloBlue());
+        set1.setLineWidth(2f);
+        set1.setCircleSize(4f);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+        // set data
+        chart.setData(data);
+    }
+}
